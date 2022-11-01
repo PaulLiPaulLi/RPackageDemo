98e286023f7452f59df7963d94665914667d1754
PaulLiPaulLi <lyb000215@gmail.com>
2022-11-01 15:45:06 -0700

    Update

--- R/square.R
+++ R/square.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param x A character vector with one element.
+#'
+#' @return split What to split on
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' square(x, split = ",")
 square <- function(x) {
   x^2
 }
